1f26f37090c74f548104d2605cb83f8cccb12323
cai4633 <463353522@qq.com>
2020-11-22 15:56:37 +0800

    button 组件

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,23 +1,131 @@
 <template>
-  <div class="m-button">
-    <slot />
-  </div>
+	<button class="c-button" :class="[size, type]" :class="{ disabled, loading }">
+		<svg class="icon" aria-hidden="true" v-if="loading">
+			<use xlink:href="#icon-loading"></use>
+		</svg>
+		<slot>Button</slot>
+	</button>
 </template>
 
 <script lang="ts">
+import { SetupContext } from "vue";
+
 interface Props {
-  type: "primary" | "simple";
+	type: "default" | "primary" | "warning" | "ghost";
+	size: "normal" | "big" | "small";
+	disabled: boolean;
+	loading: boolean;
 }
 export default {
-  props: {
-    type: { type: String, default: "primary" },
-  },
-  setup() {},
+	props: {
+		type: { type: String, default: "default" },
+		size: { type: String, default: "normal" },
+		disabled: { type: Boolean, default: false },
+		loading: { type: Boolean, default: false },
+	},
+	setup(props: Props, context: SetupContext) {},
 };
 </script>
 
 <style lang="scss">
-.m-button {
-  border: 1px solid #000;
+.c-button {
+	$w: 90px;
+	$h: 50px;
+	$bc-primary: #108ee9;
+	$bc-ghost: #fff;
+	$bc-default: #fff;
+	$bc-warning: #e94f4f;
+	$color-primary: #fff;
+	$color-ghost: $bc-primary;
+	$color-warning: #fff;
+	$color-default: #000;
+	font-size: 14px;
+	border-radius: 5px;
+	text-align: center;
+	white-space: nowrap;
+	cursor: pointer;
+	padding: 0 10px;
+	border: none;
+	display: inline-block;
+	vertical-align: middle;
+
+	&:focus {
+		border: none;
+		outline: none;
+	}
+
+	&:hover {
+		opacity: 0.8;
+		box-shadow: 0 0 2px #bbb;
+	}
+
+	& + & {
+		margin-left: 2em;
+	}
+
+	&.normal {
+		width: $w;
+		height: 0.8 * $h;
+		line-height: 0.8 * $h;
+	}
+	&.disabled {
+		cursor: not-allowed;
+	}
+
+	&.loading {
+		display: inline-flex;
+		align-items: center;
+		justify-content: center;
+
+		@keyframes myPin {
+			0% {
+				transform: rotate(0deg);
+			}
+			100% {
+				transform: rotate(360deg);
+			}
+		}
+		svg.icon {
+			display: inline-block;
+			vertical-align: top;
+			flex: 0 0 1em;
+			width: 1em;
+			height: 1em;
+			margin-right: 5px;
+			border-radius: 50%;
+			animation: myPin 1.5s linear infinite;
+		}
+	}
+	&.big {
+		width: 1.8 * $w;
+		height: $h;
+		line-height: $h;
+	}
+	&.small {
+		width: 0.8 * $w;
+		height: 0.6 * $h;
+		line-height: 0.6 * $h;
+	}
+	&.default {
+		background-color: $bc-default;
+		color: $color-default;
+		margin: 1px;
+		border: 1px solid #ddd;
+	}
+	&.primary {
+		background-color: $bc-primary;
+		color: $color-primary;
+		border: 1px solid $bc-primary;
+	}
+	&.ghost {
+		background-color: $bc-ghost;
+		color: $color-ghost;
+		border: 1px solid $color-ghost;
+	}
+	&.warning {
+		background-color: $bc-warning;
+		color: $color-warning;
+		border: 1px solid $bc-warning;
+	}
 }
 </style>
--- src/views/button-demo.vue
+++ src/views/button-demo.vue
@@ -1,14 +1,38 @@
 <template>
-  <div class="button-demo">
-    <h2>基础用法：</h2>
-    <Button>你好</Button>
-  </div>
+	<div class="button-demo">
+		<section>
+			<h3>基础用法：</h3>
+			<Button @click='onclick'>default</Button>
+		</section>
+			<h3>按钮风格 type：</h3>
+			<Button type="primary">primary</Button>
+			<Button type="ghost">ghost</Button>
+			<Button type="warning">warning </Button>
+		</section>
+		<section>
+			<h3>按钮大小 size</h3>
+			<Button type="primary" size="big">big</Button>
+			<Button type="primary" size="normal"> normal </Button>
+			<Button type="primary" size="small"> small </Button>
+		</section>
+		<section>
+			<h3>disabled 和 loading</h3>
+			<Button type="primary" disabled>disabled</Button>
+			<Button type="ghost" loading> loading </Button>
+		</section>
+	</div>
 </template>
-<script>
+<script lang='ts'>
 import Button from "../components/Button.vue";
 export default {
-  components: {
-    Button,
-  },
+	components: {
+		Button,
+	},
+  setup(props) {
+    const onclick = (e)=>{
+      console.log("click")
+    }
+    return {onclick}
+  }
 };
 </script>
