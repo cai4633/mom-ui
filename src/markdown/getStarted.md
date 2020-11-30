afe1dc4dd0d467e82a12f8f9ab341f0aa6240391
cai4633 <463353522@qq.com>
2020-12-01 01:49:42 +0800

    使用正则完善demo 代码

--- src/markdown/getStarted.md
+++ src/markdown/getStarted.md
@@ -6,9 +6,21 @@ yarn 版本： 1.22.5
 ## 快速上手
 ### 安装
 ```
-yarn add cai-ui 
+yarn add mom-ui 
 
 or 
 
-npm install cai-ui -S
+npm install mom-ui -S
+```
+
+### 使用
+在 `main.js` 中引入 css：
+```
+import "mom-ui/dist/lib/mom-ui.css";
+```
+
+在 vue 单文件组件中引入所需的UI组件：
+
+```
+import { XXX } from 'mom-ui';
 ```
--- vite.config.ts
+++ vite.config.ts
@@ -5,28 +5,34 @@
 // custom-block 是针对于自定义标签（解析<demo></demo>）而言，与文件名无必然联系（xxx.demo.vue和xxx.vue 无本质区别）
 import type { UserConfig } from "vite"
 import vitePluginVuedoc from "vite-plugin-vuedoc"
-import fs from 'fs'
-import {baseParse,TemplateNode} from '@vue/compiler-core'
+import fs from "fs"
+import { baseParse, TemplateNode } from "@vue/compiler-core"
 
 const config: UserConfig = {
   plugins: [vitePluginVuedoc()],
-  assetsDir:'assets',
-  base:'./',
-  vueCustomBlockTransforms:{
-    demo:(options)=>{
-      const {path} = options
+  assetsDir: "assets",
+  base: "./",
+  vueCustomBlockTransforms: {
+    demo: (options) => {
+      const { path } = options
       const file = fs.readFileSync(path).toString()
-      const block:any= baseParse(file).children.find((n:TemplateNode)=>n.tag==='demo') 
-      const main = file.split(block.loc.source).join('').trim()
+      const block: any = baseParse(file).children.find((n: TemplateNode) => n.tag === "demo")
+      // 将 "../libs/xxx.vue" 替换成 "mom-ui"
+      const main = file
+        .split(block.loc.source)
+        .join("")
+        .trim()
+        .replace(/import\s+(\w+)\s+from\s+["']([.]{2}\/libs\/\w+\.vue)["']/g, "import { $1 } from 'mom-ui'")
+        .replace(/[.]{2}\/libs\/\w+(?=["'])/g, "mom-ui")
+
+      console.log(main)
+
       return `export default function (Component){
-        Component._source = ${
-          JSON.stringify(main)
-        };
+        Component._source = ${JSON.stringify(main)};
         Component._title = ${JSON.stringify(block.children[0].content)} //必须使用JSON.stringify()转换JS表达式，否则报错
       }`.trim()
-      
-    }
-  }
+    },
+  },
 }
 
 export default config
